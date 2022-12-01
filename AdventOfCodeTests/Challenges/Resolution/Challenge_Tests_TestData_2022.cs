1bc5551f8b4a34f038e68e6d9f0ac24f903a196c
Kurtis Mackey <tsunamicom2k7@gmail.com>
2022-12-01 00:35:18 -0500

    Complete 2022 Day 1

--- AdventOfCodeTests/Challenges/Resolution/Challenge_Tests_TestData_2022.cs
+++ AdventOfCodeTests/Challenges/Resolution/Challenge_Tests_TestData_2022.cs
@@ -6,7 +6,20 @@ namespace AdventOfCode.Challenges.Resolution.Tests
     {
         private List<string> _TestData_2022_01 = new()
         {
-
+            "1000",
+            "2000",
+            "3000",
+            "",
+            "4000",
+            "",
+            "5000",
+            "6000",
+            "",
+            "7000",
+            "8000",
+            "9000",
+            "",
+            "10000",
         };
         private List<string> _TestData_2022_02 = new()
         {
