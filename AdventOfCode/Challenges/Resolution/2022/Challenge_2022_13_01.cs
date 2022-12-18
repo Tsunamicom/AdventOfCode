e6ba05163849ccd824140fbee0d518e30d35d189
Kurtis Mackey <tsunamicom2k7@gmail.com>
2022-12-18 02:36:24 -0500

    Incomplete Day 13-1

--- AdventOfCode/Challenges/Resolution/2022/Challenge_2022_13_01.cs
+++ AdventOfCode/Challenges/Resolution/2022/Challenge_2022_13_01.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json;
 
 namespace AdventOfCode.Challenges.Resolution
 {
@@ -10,7 +11,62 @@ namespace AdventOfCode.Challenges.Resolution
 
         public string ResolveChallenge(List<string> data)
         {
+            var itemSets = GetInitialGroups(data);
+
+            var jdoc1 = JsonDocument.Parse("[[[]]]");
+
+            var j = jdoc1.RootElement.EnumerateArray();
+            foreach (var x in j)
+            {
+                var t = x.ValueKind;
+                if (t == JsonValueKind.Array)
+                {
+                    var z = x.EnumerateArray();
+                    foreach (var y in z)
+                    {
+                        var ty = y.ValueKind;  // Reveals type Array or Number
+                    }
+                }
+            }
+
+
+
             return "Not Implemented Yet";
         }
+
+        private List<(string, string)> GetInitialGroups(List<string> data)
+        {
+            var itemSets = new List<(string, string)>();
+
+            string left = null;
+            string right = null;
+
+            foreach (var line in data)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    itemSets.Add((left, right));
+                    left = right = null;
+                    continue;
+                }
+
+                if (string.IsNullOrEmpty(left))
+                {
+                    left = line;
+                    continue;
+                }
+
+                if (string.IsNullOrEmpty(right))
+                {
+                    right = line;
+                    continue;
+                }
+            }
+            itemSets.Add((left, right));
+
+            return itemSets;
+        }
     }
+
+
 }
