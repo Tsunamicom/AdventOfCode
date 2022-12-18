4baeaf3c6384a4cd17c48321a6c048bd3ac6064b
Kurtis Mackey <tsunamicom2k7@gmail.com>
2022-12-18 02:38:03 -0500

    Cleanup Day 17 references

--- AdventOfCode/Challenges/Resolution/2022/Challenge_2022_17_01.cs
+++ AdventOfCode/Challenges/Resolution/2022/Challenge_2022_17_01.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.VisualBasic;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System;
-using System.Diagnostics;
 
 namespace AdventOfCode.Challenges.Resolution
 {
